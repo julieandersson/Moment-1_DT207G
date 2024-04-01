9adc071ff9b62b608a9a1b7fc920f83c00635773
julieandersson <juan2301@student.miun.se>
2024-04-01 23:18:17 +0300

    skapar en ny vy för att redigera kursinformation, kör dbrun UPDATE för att spara ändringar

--- server.js
+++ server.js
@@ -88,10 +88,10 @@ app.post("/create", (req, res) => {
     }
 });
 
+//Radera specifikt inlägg
 app.get("/delete/:id", (req, res) => {
     let id = req.params.id;
 
-    //Radera specifikt inlägg
     db.run("DELETE FROM courses WHERE id=?;", id, (err) => {
         if(err) {
             console.error(err.message);
@@ -100,6 +100,73 @@ app.get("/delete/:id", (req, res) => {
         //Redirect till startsidan
         res.redirect("/");
     });
+});
+
+// Ändra kursinformation för specifik kurs
+app.get("/editcourse/:id", (req, res) => {
+    let id = req.params.id;
+
+    db.get("SELECT * FROM courses WHERE id=?;", id, (err, row) => {
+        if (err) {
+            console.error(err.message);
+        }
+
+        res.render("editcourse", {
+            row: row,
+            error: "",
+            progression: row.progression
+        })
+    });
+});
+
+app.post("/editcourse/:id", (req,res) => {
+    let id = req.params.id;
+    let coursecode = req.body.coursecode;
+    let coursename = req.body.coursename;
+    let syllabus = req.body.syllabus;
+    let progression = req.body.progression;
+
+    errors = [];
+
+    if (coursecode === "") {
+        errors.push("Ange en korrekt kurskod");
+    }
+
+    if (coursename === "") {
+        errors.push("Ange ett korrekt kursnamn");
+    }
+
+    if (syllabus === "") {
+        errors.push("Ange en länk till kursplanen");
+    }
+
+    if (progression === "") {
+        errors.push("Ange progression");
+    }
+
+        // Om det finns några fel, rendera editcourse med felmeddelanden
+        if (errors.length > 0) {
+            res.render("editcourse", {
+                row: {
+                    id: id,
+                    coursecode: coursecode,
+                    coursename: coursename,
+                    syllabus: syllabus,
+                    progression: progression
+                },
+                error: errors
+            });
+    // Uppdatera kursen i databasen
+    } else {
+        db.run("UPDATE courses SET coursecode=?, coursename=?, syllabus=?, progression=? WHERE id=?", [coursecode, coursename, syllabus, progression, id], (err) => {
+            if (err) {
+                console.error(err.message);
+            } else {
+                res.redirect("/");
+            }
+
+    });
+    }
 });
 
 // Hämtar om-sidan
