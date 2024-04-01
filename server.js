9797a2d3521be3aa68642370ed6f339cfd8a4898
julieandersson <juan2301@student.miun.se>
2024-04-01 17:44:36 +0300

    skapar innehåll på ejssidorna, formulär för att lägga till ny kurs, hämtar sidorna med routing

--- server.js
+++ server.js
@@ -2,7 +2,39 @@ const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("./db/cv.db");
 
 const express = require("express");
+const bodyParser = require("body-parser"); // läser in formulärdatan
 const app = express();
 const port = 3000;
+
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static("public")); // Statiska filer
+app.use(bodyParser.urlencoded({extended: true }));
+
+/**
+ * Routing
+ */
+
+// Hämtar startsidan 
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+// Hämtar sida för att lägga till ny kurs
+app.get("/create", (req, res) => {
+
+    res.render("create");
+});
+
+app.post("/create", (req, res) => {
+    res.render("create");
+})
+
+// Hämtar om-sidan
+app.get("/aboutsite", (req, res) => {
+    res.render("aboutsite");
+});
+
+// Starta
+app.listen(port, () => {
+    console.log("Server startad på port: " + port);
+})
